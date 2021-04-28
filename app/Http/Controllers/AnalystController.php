bb1bbe442ae3647e9a72a16968aa2845d764bc68
Phillip Navarrete <phillipvictor2000@gmail.com>
2021-04-29 00:03:42 +0100

    Added graphs to analyst dashboard

--- app/Http/Controllers/AnalystController.php
+++ app/Http/Controllers/AnalystController.php
@@ -3,18 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Models\Problem;
+use App\Models\Employee;
 use App\Models\Hardware;
 use App\Models\Software;
+use App\Models\ProblemLog;
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 
 class AnalystController extends Controller
 {
 
     public function index(){
+
+        $hardware = Hardware::has('problemlog')->withCount('problemlog')->get();
+        $software = Software::has('problemlog')->withCount('problemlog')->get();
+        $types = Problem::has('problemLogs')->withCount('problemLogs')->get();
+        $specialists = Employee::has('specialist')->get();
+
+        $names = [];
+        $solved = [];
+        $queue = [];
+        $verify = [];
+
+        foreach($specialists as $specialist){
+            $names = Arr::add($names,$specialist->id,$specialist->forename." ".$specialist->surname);
+            $problemlogs = ProblemLog::selectRaw('count(*) as total, status')->groupBy('status')->with('trackers')->whereHas('trackers',function($query) use ($specialist){
+                $query->where('employee_id',$specialist->id);
+            })->get();
+
+            foreach($problemlogs as $log){
+                if($log->status == "Verify"){
+                    $verify = Arr::add($verify, $specialist->id,$log->total);
+                }else if($log->status == "In queue"){
+                    $queue = Arr::add($queue, $specialist->id,$log->total);
+                }else if($log->status == "Solved"){
+                    $solved = Arr::add($solved, $specialist->id,$log->total);
+                }
+            }
+        }
+
         return view(
             "analyst.analyst_dashboard",
             [
-               "navTitle" => "dashboard"
+               "navTitle" => "dashboard",
+               'hardware' => [
+                    "labels" => $hardware->pluck('serial_num'),
+                    "qty" => $hardware->pluck('problemlog_count')
+               ],
+               'software' => [
+                    "labels" => $software->pluck('name'),
+                    "qty" => $software->pluck('problemlog_count')
+               ],
+               'problem' => [
+                    "labels" => $types->pluck('problem_type'),
+                    "qty" => $types->pluck('problem_logs_count')
+               ],
+               "specialist" => [
+                    "labels" => array_values($names),
+                    "solved" => array_values($solved),
+                    "queue" => array_values($queue),
+                    "verify" => array_values($verify),
+               ]
             ]
         );
     }
--- resources/views/analyst/analyst_dashboard.blade.php
+++ resources/views/analyst/analyst_dashboard.blade.php
@@ -4,6 +4,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
         body{
             background-color: #F4F5F7;
@@ -35,7 +36,7 @@
 
                 <div class="stats-card">
                     <h4> Stats </h4>
-                    <h3>   </h3>   
+                    <h3>   </h3>
                 </div>
             </div>
         </div>
@@ -48,7 +49,7 @@
         <br><br>
         <div class="stats-card-section">
             <h2> Monthly Data </h2>
-            
+
             <div class="stats-card-container-analyst">
                 <div class="stats-card">
                 </div>
@@ -63,9 +64,99 @@
         <br>
         <div class="stats-card-section">
             <h2> Yearly Data </h2>
-            
+
             <div class="stats-card">
             </div>
+        </div>
+
         <div>
+            <canvas id="hardwareChart"></canvas>
+        </div>
+
+        <div>
+            <canvas id="softwareChart"></canvas>
+        </div>
+
+        <div>
+            <canvas id="typesChart"></canvas>
+        </div>
+
+        <div>
+            <canvas id="specialistChart"></canvas>
+        </div>
+
+        <script>
+
+            var hardwareChart = new Chart( document.getElementById('hardwareChart'),{
+                type: 'bar',
+                data: {
+                    labels: {!!json_encode($hardware['labels'])!!},
+                    datasets: [{
+                        label: "Hardware problems",
+                        data: {!!json_encode($hardware['qty'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                }
+            });
+
+            var softwareChart = new Chart( document.getElementById('softwareChart'),{
+                type: 'bar',
+                data: {
+                    labels: {!!json_encode($software['labels'])!!},
+                    datasets: [{
+                        label: "Software problems",
+                        data: {!!json_encode($software['qty'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                }
+            });
+
+            var typesChart = new Chart( document.getElementById('typesChart'),{
+                type: 'bar',
+                data: {
+                    labels: {!!json_encode($problem['labels'])!!},
+                    datasets: [{
+                        label: "Problem Type problems",
+                        data: {!!json_encode($problem['qty'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                }
+            });
+
+            var specialistChart = new Chart( document.getElementById('specialistChart'),{
+                type: 'bar',
+                data: {
+                    labels: {!!json_encode($specialist['labels'])!!},
+                    datasets: [{
+                        label: "Solved problems",
+                        data: {!!json_encode($specialist['solved'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    },{
+                        label: "Queued problems",
+                        data: {!!json_encode($specialist['queue'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    },{
+                        label: "Verify problems",
+                        data: {!!json_encode($specialist['verify'])!!},
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    }]
+                }
+            });
+
+        </script>
+
+    </div>
 
 @endsection
