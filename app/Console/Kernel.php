59df233416c9ea818f50f1d69fbb0d6e74913047
Phillip Navarrete <phillipvictor2000@gmail.com>
2021-05-07 11:28:57 +0100

    Added Scheduled task to update specialist availability

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use App\Models\Holiday;
+use App\Models\Employee;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +27,29 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(
+            function(){
+                $now = Carbon::now()->toArray();
+                $specialists = Employee::has('specialist');
+                foreach($specialists as $spec){
+                    $days = explode(",",$spec->specialist->working_days);
+                    $in = false;
+                    foreach($days as $day){
+                        if($day == substr(strtolower($now['englishDayOfWeek']), 0, 2)){
+                            $in = true;
+                            break;
+                        }
+                    }
+                    $holidays = Holiday::where('employee_id', $spec->id)->whereRaw('? between start_date and end_date',[Carbon::now()]);
+                    if($holidays == null && $in){
+                        $spec->specialist->is_available = true;
+                    } else {
+                        $spec->specialist->is_available = false;
+                    }
+                    $spec->specialist->save();
+                }
+            }
+        )->daily();
     }
 
     /**
